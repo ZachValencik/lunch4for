19178a26710545045b87e9a1cbb4e2891b4728ad
Carlos <galvancarlosjr@gmail.com>
2020-12-06 14:55:46 -0600

    Working Admin Account table
    You can now edit users status

--- util/formatActivity.js
+++ util/formatActivity.js
@@ -1,10 +1,24 @@
 let updateActivity  =  (id_list = [], activity_list) => {
     console.log(id_list, activity_list)
+    let temp = []
+    for(let i = 0; i < activity_list.length; i += 2){
+        console.log(i,activity_list[i])
+        if(activity_list[i] === '1'){
+            temp.push(activity_list[i])
+            // i++
+        }else if (activity_list[i] === '0'){
+            temp.push(activity_list[i])
+            i--
+        }
+    }
+    console.log('Temp:', temp)
+    // activity_list
 
     let formated = []
     let count = 0
+
     id_list.forEach((id) => {
-        formated.push({id : id, active : activity_list[count]})
+        formated.push({id : id, active : temp[count]})
         count++
     })
 
